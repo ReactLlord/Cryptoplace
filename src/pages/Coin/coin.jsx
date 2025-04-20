0f64c4c7d94e72a197c31c530d56aa5f46c835f0
EBUNOLUWA <141589270+ReactLlord@users.noreply.github.com>
2025-04-20 07:13:27 +0100

    Update coin.jsx

--- src/pages/Coin/coin.jsx
+++ src/pages/Coin/coin.jsx
@@ -4,95 +4,96 @@ import { useParams } from 'react-router-dom'
 import { CoinContext } from '../../context/CoinContext';
 import LineChart from '../../components/LineChart/LineChart';
 
-const coin = () => {
-
-  const {coinId} = useParams();
-  const [coinData, setCoinData] = useState();
-  const [historicalData, setHistoricalData] = useState();
-  const {currency} = useContext(CoinContext)
+const Coin = () => {
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState(null);
+  const [historicalData, setHistoricalData] = useState(null);
+  const { currency } = useContext(CoinContext);
 
   const fetchCoinData = async () => {
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        'x-cg-demo-api-key': '	CG-bXGDYeB1CD4RTdTXpAZDE7UX'
+        'x-cg-demo-api-key': 'CG-bXGDYeB1CD4RTdTXpAZDE7UX'
       }
     };
-    
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
-      .then(res => res.json())
-      .then(res => setCoinData(res))
-      .catch(err => console.error(err));
-  }
 
-  const fetchHistoricalData = async => {
+    try {
+      const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options);
+      const data = await res.json();
+      setCoinData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const fetchHistoricalData = async () => {
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        'x-cg-demo-api-key': '	CG-bXGDYeB1CD4RTdTXpAZDE7UX'
+        'x-cg-demo-api-key': 'CG-bXGDYeB1CD4RTdTXpAZDE7UX'
       }
     };
-    
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
-      .then(res => res.json())
-      .then(res => setHistoricalData(res))
-      .catch(err => console.error(err));
-  }
 
-  useEffect(()=>{
+    try {
+      const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options);
+      const data = await res.json();
+      setHistoricalData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
     fetchCoinData();
     fetchHistoricalData();
-  }, [currency])
+  }, [currency]);
 
-if (coinData, historicalData){
-  return (
-    <div className='coin'>
+  if (coinData && historicalData) {
+    return (
+      <div className='coin'>
         <div className="coin-name">
-          <img src={coinData.image.large} alt="" />
+          <img src={coinData?.image?.large} alt={coinData?.name} />
           <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
         </div>
-        
+
         <div className="coin-chart">
-          <LineChart historicalData={historicalData}/>
+          <LineChart historicalData={historicalData} />
         </div>
-          
-          <div className="coin-info">
-            <ul>
-              <li>Crypto Market Rank</li>
-              <li>{coinData.market_cap_rank}</li>
-            </ul>
-            <ul>
-              <li>Current Price</li>
-              <li>{currency.symbol} {coinData.market_data.current_price [currency.name]. toLocaleString()}</li>
-            </ul>
-            <ul>
-              <li>Market Cap</li>
-              <li>{currency.symbol} {coinData.market_data.market_cap [currency.name]. toLocaleString()}</li>
-            </ul>
-            <ul>
-              <li>24 Hour high</li>
-              <li>{currency.symbol} {coinData.market_data.high_24h [currency.name]. toLocaleString()}</li>
-            </ul>
-            <ul>
-              <li>24 Hour Low</li>
-              <li>{currency.symbol} {coinData.market_data.low_24h [currency.name]. toLocaleString()}</li>
-            </ul>
-          </div>
-    </div>
-  )
-}else{
-  return (
- 
-       <div className="spinner">
+
+        <div className="coin-info">
+          <ul>
+            <li>Crypto Market Rank</li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
+          <ul>
+            <li>Current Price</li>
+            <li>{currency.symbol} {coinData.market_data.current_price[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>Market Cap</li>
+            <li>{currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>24 Hour high</li>
+            <li>{currency.symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>24 Hour Low</li>
+            <li>{currency.symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="spinner">
         <div className="spin"></div>
-       </div>
-  
-  )
-}
+      </div>
+    );
+  }
+};
 
- 
-}
-
-export default coin
+export default Coin;
